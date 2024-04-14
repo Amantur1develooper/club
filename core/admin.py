07ae2376a936b544f1d0c0dcda832fe23549b7ee
Амантур Эркинов <amanturerkinov@Noutbuk-Amantur.local>
2024-04-14 16:29:00 +0600

    1commit

---
+++ core/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+admin.site.register(User)
+admin.site.register(Documentation)
+admin.site.register(Topics_Documentation)
+admin.site.register(Topics_Test)
+admin.site.register(Test)
+admin.site.register(AboutUs)
